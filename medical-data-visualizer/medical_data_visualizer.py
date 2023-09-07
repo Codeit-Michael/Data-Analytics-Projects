76c34dc07ce54b233e0c3ca3c8175106d06ae7bf
Codeit.Michael <michaelmaranan030@gmail.com>
2023-09-07 20:38:57 +0800

    add medical_data_visualizert

--- medical-data-visualizer/medical_data_visualizer.py
+++ medical-data-visualizer/medical_data_visualizer.py
@@ -4,57 +4,56 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Import data
-df = None
+df = pd.read_csv("medical_examination.csv")
 
 # Add 'overweight' column
-df['overweight'] = None
+conditions = [
+  df["weight"] / ((df["height"] * 0.01)**2) <= 25,
+  df["weight"] / ((df["height"] * 0.01)**2) > 25
+]
+choices = [0, 1]
+
+df["overweight"] = np.select(conditions, choices, default=0)
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
 
 
 # Draw Categorical Plot
 def draw_cat_plot():
-    # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
+  # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
+  df_cat = pd.melt(df, value_vars=["cholesterol", "gluc", "smoke", "alco", "active", "overweight"])
+
+  # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
+  df_cat = None
+
+  # Draw the catplot with 'sns.catplot()'
 
 
-    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
-    
+  # Get the figure for the output
+  fig = None
 
-    # Draw the catplot with 'sns.catplot()'
-
-
-
-    # Get the figure for the output
-    fig = None
-
-
-    # Do not modify the next two lines
-    fig.savefig('catplot.png')
-    return fig
+  # Do not modify the next two lines
+  fig.savefig('catplot.png')
+  return fig
 
 
 # Draw Heat Map
 def draw_heat_map():
-    # Clean the data
-    df_heat = None
+  # Clean the data
+  df_heat = None
 
-    # Calculate the correlation matrix
-    corr = None
+  # Calculate the correlation matrix
+  corr = None
 
-    # Generate a mask for the upper triangle
-    mask = None
+  # Generate a mask for the upper triangle
+  mask = None
 
+  # Set up the matplotlib figure
+  fig, ax = None
 
-
-    # Set up the matplotlib figure
-    fig, ax = None
-
-    # Draw the heatmap with 'sns.heatmap()'
+  # Draw the heatmap with 'sns.heatmap()'
 
 
-
-    # Do not modify the next two lines
-    fig.savefig('heatmap.png')
-    return fig
+  # Do not modify the next two lines
+  fig.savefig('heatmap.png')
+  return fig
